ee2b7717b1d0a91eb2a689bb4fed9b1a881acf92
Vhmed <vhmdhoss@gmail.com>
2025-04-12 00:47:08 +0200

    Major changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import Layout from "./Layout/Layout";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router";
 export default function App() {
+  const spaceTourism = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [{}, {}, {}, {}],
+    },
+  ]);
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <RouterProvider router={spaceTourism} />
+    </>
+  );
 }
