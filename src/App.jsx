ffd2ba5059dcbd1e6cfbf3f857df85feaea4f0b5
Vhmed <vhmdhoss@gmail.com>
2025-04-13 01:04:28 +0200

    Some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import Layout from "./Layout/Layout";
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router";
 import Home from "./Pages/Home/home";
-import Destinations from "./Pages/Destinations/destinations";
-import Crew from "./Pages/Crew/crew";
-import Technology from "./Pages/Technology/technology";
 import Aos from "aos";
 import { Navigate } from "react-router-dom";
+import Destinations from "./Pages/Destinations/destinations";
+import Technology from "./Pages/Technology/technology";
+import Crew from "./Pages/Crew/crew";
 export default function App() {
   const spaceTourism = createBrowserRouter([
     {
--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
-
+import React from "react";
+import "./home.css";
 export default function Home() {
   return (
-<div
-  className="h-screen bg-cover bg-center bg-no-repeat"
-  style={{ backgroundImage: "url('/Images/Hi%20Res%20Render%20-%201.png')" }}
->
-
-</div>
-
-  )
+    <div className="h-screen bg-cover bg-center bg-no-repeat bg-img"></div>
+  );
 }
