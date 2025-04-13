4b2edf41976210df5738dd5848583dc5356ba50d
Vhmed <vhmdhoss@gmail.com>
2025-04-13 02:30:57 +0200

    Adding Router Link

--- src/Components/Sphere/sphere.jsx
+++ src/Components/Sphere/sphere.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Sphere() {
   return (
     <div className="group rounded-full relative">
       <span className="absolute w-[125%] h-[125%] z-[-1] rounded-full bg-white left-[-12.5%] top-[-12.5%] opacity-0 group-hover:opacity-20 transition-all duration-500 ease-in-out" />
-      <div className="flex items-center justify-center rounded-full max-sm:size-[144px] size-[272px] bg-white text-black">
+     <Link to="/destinations"> <div className="flex items-center justify-center rounded-full max-sm:size-[144px] size-[272px] bg-white text-black">
         <h2 className="font-bellefair text-[32px] uppercase max-sm:text-[18px]">Explore</h2>
-      </div>
+      </div></Link>
     </div>
   );
 }
