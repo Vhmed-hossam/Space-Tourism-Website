8c0d9a868e231758443d97e97748ee8791e90b5d
Vhmed <vhmdhoss@gmail.com>
2025-04-13 20:14:37 +0200

    Adding image to data

--- src/Constants/Destinationtext.js
+++ src/Constants/Destinationtext.js
@@ -2,11 +2,12 @@ export const Destinationtext = [
   {
     id: 1,
     title: "Moon",
+    image: "/Kit/Destination - A - Moon.png",
     about: `See our planet as you’ve never seen it before.
-     A perfect relaxing trip away to help regain 
-     perspective and come back refreshed. While you’re there, 
-     take in some history by visiting the Luna 2 and Apollo 11 
-     landing sites.`,
+       A perfect relaxing trip away to help regain 
+       perspective and come back refreshed. While you’re there, 
+       take in some history by visiting the Luna 2 and Apollo 11 
+       landing sites.`,
     data: [
       {
         text: "Avg. distance",
@@ -21,9 +22,8 @@ export const Destinationtext = [
   {
     id: 2,
     title: "Mars",
-    about: `Don’t forget to pack your hiking boots.
-     You’ll need them to tackle Olympus Mons, the tallest planetary mountain 
-    in our solar system. It’s two and a half times the size of Everest!`,
+    image: "/Kit/Destination - B - Mars.png",
+    about: `Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!`,
     data: [
       {
         text: "Avg. distance",
@@ -38,10 +38,11 @@ export const Destinationtext = [
   {
     id: 3,
     title: "Europa",
+    image: "/Kit/Destination - C - Europa.png",
     about: `The smallest of the four Galilean moons
-     orbiting Jupiter, Europa is a winter lover’s dream.
-      With an icy surface, it’s perfect for a bit of ice skating
-    , curling, hockey, or simple relaxation in your snug wintery cabin.`,
+       orbiting Jupiter, Europa is a winter lover’s dream.
+        With an icy surface, it’s perfect for a bit of ice skating
+      , curling, hockey, or simple relaxation in your snug wintery cabin.`,
     data: [
       {
         text: "Avg. distance",
@@ -56,10 +57,12 @@ export const Destinationtext = [
   {
     id: 4,
     title: "Titan",
+    image: "/Kit/Destination - D - Titan.png",
+    extra: `(just a few hundred degrees colder!). `,
     about: `The only moon known to have a
-     dense atmosphere other than Earth, Titan is a 
-     home away from home (just a few hundred degrees colder!). 
-    As a bonus, you get striking views of the Rings of Saturn.`,
+       dense atmosphere other than Earth, Titan is a 
+       home away from home (just a few hundred degrees colder!). 
+      As a bonus, you get striking views of the Rings of Saturn.`,
     data: [
       {
         text: "Avg. distance",
