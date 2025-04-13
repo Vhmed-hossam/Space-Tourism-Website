3d135461dce48aa888d5c9a53d071d285df99b33
Vhmed <vhmdhoss@gmail.com>
2025-04-14 00:16:34 +0200

    Fixing numbers

--- src/static/technology.js
+++ src/static/technology.js
@@ -1,10 +1,16 @@
 export const SL101 = {
-    count: "03",
-   text: 'space launch 101',
-   auto: 'the terminology... '
-}
-export const TechNumbers = {
-    one: 1,
-    two: 2,
-    three: 3
-}
+  count: "03",
+  text: "space launch 101",
+  auto: "the terminology... ",
+};
+export const TechNumbers = [
+  {
+    number: 1,
+  },
+  {
+    number: 2,
+  },
+  {
+    number: 3,
+  },
+];
