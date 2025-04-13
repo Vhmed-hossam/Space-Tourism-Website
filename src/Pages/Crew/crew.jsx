fa8c2467a717aad89911f242628b40a0670b7c25
Vhmed <vhmdhoss@gmail.com>
2025-04-13 21:16:06 +0200

    Setting up Crew

--- src/Pages/Crew/crew.jsx
+++ src/Pages/Crew/crew.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import NavBar from "../../Components/Navbar/navbar";
+import "./crew.css";
 export default function Crew() {
   return (
-    <div>
-      crew
+    <div className="bg-img3 h-auto overflow-hidden">
+      <NavBar />
+      <div className="bg-red-400 flex-1 self-stretch items-center justify-center text-white">
+        
+      </div>
     </div>
-  )
+  );
 }
