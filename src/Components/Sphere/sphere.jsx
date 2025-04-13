7734b65ccbfa1e99ef060f059652c30977081dbc
Vhmed <vhmdhoss@gmail.com>
2025-04-13 02:31:36 +0200

    Fixing link

--- src/Components/Sphere/sphere.jsx
+++ src/Components/Sphere/sphere.jsx
@@ -5,7 +5,7 @@ export default function Sphere() {
   return (
     <div className="group rounded-full relative">
       <span className="absolute w-[125%] h-[125%] z-[-1] rounded-full bg-white left-[-12.5%] top-[-12.5%] opacity-0 group-hover:opacity-20 transition-all duration-500 ease-in-out" />
-     <Link to="/destinations"> <div className="flex items-center justify-center rounded-full max-sm:size-[144px] size-[272px] bg-white text-black">
+     <Link to="/destination"> <div className="flex items-center justify-center rounded-full max-sm:size-[144px] size-[272px] bg-white text-black">
         <h2 className="font-bellefair text-[32px] uppercase max-sm:text-[18px]">Explore</h2>
       </div></Link>
     </div>
