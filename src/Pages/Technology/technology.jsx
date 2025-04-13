a9cc49f332d4285a30abced5ed94c0633289e0bb
Vhmed <vhmdhoss@gmail.com>
2025-04-13 23:12:10 +0200

    Setting up Tech

--- src/Pages/Technology/technology.jsx
+++ src/Pages/Technology/technology.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React, { useRef } from "react";
+import NavBar from "../../Components/Navbar/navbar";
+import './tech.css';
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 export default function Technology() {
+  const formRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.fromTo(
+      formRef.current,
+      { opacity: 0, y: 40 },
+      { opacity: 1, duration: 0.7, ease: "power1.out", y: 0 }
+    );
+  });
   return (
-    <div>
-      
+    <div className="bg-img4 h-auto overflow-hidden transition-all">
+      <NavBar />
+      <div ref={formRef}>
+
+      </div>
     </div>
-  )
+  );
 }
