5352fdd88a57abf2f21558cb56f249d20e66d60e
Vhmed <vhmdhoss@gmail.com>
2025-04-13 21:30:53 +0200

    Setting up crew Data

---
+++ src/Constants/Crewtext.js
@@ -0,0 +1,36 @@
+export const CrewTest = [
+  {
+    crew: "Douglas Hurley",
+    role: "Commander",
+    image: "/Extra/Crew/Crew 1.png",
+    about: `Douglas Gerald Hurley is an American engineer,
+       former Marine Corps pilot and former
+       NASA astronaut. He launched into space for
+        the third time as commander of Crew Dragon Demo-2.`,
+  },
+  {
+    crew: "Mark Shuttleworth",
+    role: "Mission Specialist",
+    image: "/Extra/Crew/Crew 2.png",
+    about: `Mark Richard Shuttleworth is the founder and CEO of Canonical,
+       the company behind the Linux-based Ubuntu operating system. 
+       Shuttleworth became the first South African to travel to space as a space tourist.`,
+  },
+  {
+    crew: "Victor Glover",
+    role: "Pilot",
+    image: "/Extra/Crew/Crew 3.png",
+    about: `Pilot on the second flight of the SpaceX Crew Dragon to the 
+      International Space Station. Glover is a commander in the U.S. 
+      Navy where he pilots an F/A-18.He was a crew member of Expedition 64, 
+      and served as a station systems flight engineer.`,
+  },
+  {
+    crew: "Anousheh Ansari",
+    role: "Flight Engineer",
+    image: "/Extra/Crew/Crew 4.png",
+    about: `Anousheh Ansari is an Iranian American engineer and co-founder 
+      of Prodea Systems. Ansari was the fourth self-funded space tourist,
+       the first self-funded woman to fly to the ISS, and the first Iranian in space.`,
+  },
+];
