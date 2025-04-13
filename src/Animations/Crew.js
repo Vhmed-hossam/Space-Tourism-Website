aead8bbe2a49c1b4788c3d90b1764e2ee94eca36
Vhmed <vhmdhoss@gmail.com>
2025-04-13 22:40:33 +0200

    more animation fixes

--- src/Animations/Crew.js
+++ src/Animations/Crew.js
@@ -1,13 +1,14 @@
 export const CrewAnimationSettings = {
-  initial: { opacity: 0, x: 70 },
-  animate: { opacity: 1, x: 0 },
-  exit: { opacity: 0, x: 70, scale: 0.9 },
-  transition: { duration: 0.7 },
-};
-
-export const textAnimationSettings = {
-  initial: { opacity: 0, y: 20 },
-  animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: -20 },
-  transition: { duration: 0.5 },
-};
+    initial: { opacity: 0, y: 70 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -70 },
+    transition: { duration: 0.7 },
+  };
+  
+  export const textAnimationSettings = {
+    initial: { opacity: 0, y: 20 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -20 },
+    transition: { duration: 0.5 },
+  };
+  
