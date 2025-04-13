408993e5d70240e8e526dbf797e09cfb91b18d5a
Vhmed <vhmdhoss@gmail.com>
2025-04-13 18:25:48 +0200

    Setting up data and importing it

---
+++ src/Constants/Destinationtext.js
@@ -0,0 +1,74 @@
+export const Destinationtext = [
+  {
+    id: 1,
+    title: "Moon",
+    about: `See our planet as you’ve never seen it before.
+     A perfect relaxing trip away to help regain 
+     perspective and come back refreshed. While you’re there, 
+     take in some history by visiting the Luna 2 and Apollo 11 
+     landing sites.`,
+    data: [
+      {
+        text: "Avg. distance",
+        value: "384,400 km",
+      },
+      {
+        text: "Est. travel time",
+        value: "3 days",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Mars",
+    about: `Don’t forget to pack your hiking boots.
+     You’ll need them to tackle Olympus Mons, the tallest planetary mountain 
+    in our solar system. It’s two and a half times the size of Everest!`,
+    data: [
+      {
+        text: "Avg. distance",
+        value: "225 MIL. km",
+      },
+      {
+        text: "Est. travel time",
+        value: "9 months",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Europa",
+    about: `The smallest of the four Galilean moons
+     orbiting Jupiter, Europa is a winter lover’s dream.
+      With an icy surface, it’s perfect for a bit of ice skating
+    , curling, hockey, or simple relaxation in your snug wintery cabin.`,
+    data: [
+      {
+        text: "Avg. distance",
+        value: "628 MIL. km",
+      },
+      {
+        text: "Est. travel time",
+        value: "3 years",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Titan",
+    about: `The only moon known to have a
+     dense atmosphere other than Earth, Titan is a 
+     home away from home (just a few hundred degrees colder!). 
+    As a bonus, you get striking views of the Rings of Saturn.`,
+    data: [
+      {
+        text: "Avg. distance",
+        value: "1.6 BIL. km",
+      },
+      {
+        text: "Est. travel time",
+        value: "7 years",
+      },
+    ],
+  },
+];
--- src/Pages/Destinations/destinations.jsx
+++ src/Pages/Destinations/destinations.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./destination.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import NavBar from "../../Components/Navbar/navbar";
+import { Destinationtext } from "../../Constants/Destinationtext";
 export default function Destinations() {
-  return(
-     <div className="bg-img2">
-      Destinations
+  const formRef = useRef(null);
+  useGSAP(() => {
+    gsap.fromTo(
+      formRef.current,
+      { opacity: 0, y: 100 },
+      { opacity: 1, duration: 0.7, ease: "power1.out", y: 0 }
+    );
+  });
+  return (
+    <div className="bg-img2 h-screen ">
+      <NavBar />
+      <div className="flex flex-1 gap-2 items-center self-stretch flex-col px-10 py-12">
+        <div className="flex flex-col flex-1  self-stretch text-white container mx-auto align-start gap-6">
+          <div></div>
+          <div></div>
+        </div>
       </div>
-  )
+    </div>
+  );
 }
