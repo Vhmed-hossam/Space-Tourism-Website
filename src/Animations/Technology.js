10e5a6eefd6a886b6bab8aa979eb7fc57c6a0385
Vhmed <vhmdhoss@gmail.com>
2025-04-14 05:18:20 +0200

    Making the animation more effective

--- src/Animations/Technology.js
+++ src/Animations/Technology.js
@@ -1,13 +1,13 @@
 export const footageAnimationSettings = {
-  initial: { opacity: 0, y: 20 },
+  initial: { opacity: 0, y: 50 },
   animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: -20 },
+  exit: { opacity: 0, y: -50 },
   transition: { duration: 0.7 },
 };
 
 export const textAnimationSettings = {
-  initial: { opacity: 0, y: 20 },
+  initial: { opacity: 0, y: 30 },
   animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: -20 },
+  exit: { opacity: 0, y: -30 },
   transition: { duration: 0.6 },
 };
