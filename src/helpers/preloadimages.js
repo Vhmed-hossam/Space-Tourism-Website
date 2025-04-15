129600f050cc08f3422027cc5d9d559697c5e9f9
Vhmed <vhmdhoss@gmail.com>
2025-04-15 02:13:16 +0200

    image loader improvement

--- src/helpers/preloadimages.js
+++ src/helpers/preloadimages.js
@@ -1,13 +1,12 @@
 export const preloadImages = (imageUrls) => {
-    return Promise.all(
-      imageUrls.map((url) => {
-        return new Promise((resolve) => {
-          const img = new Image();
-          img.src = url;
-          img.onload = resolve;
-          img.onerror = resolve; // still resolve to avoid blocking
-        });
-      })
-    );
-  };
-  
+  return Promise.all(
+    imageUrls.map((url) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = url;
+        img.onload = () => resolve(img);
+        img.onerror = (err) => reject(new Error(`Failed to load image: ${url}`));
+      });
+    })
+  );
+};
