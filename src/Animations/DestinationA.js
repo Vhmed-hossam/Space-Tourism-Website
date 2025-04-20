91948fdbc50e69cbb4147fb455fee1c7fc1b7ff2
Vhmed <vhmdhoss@gmail.com>
2025-04-20 22:53:02 +0200

    improving animations

--- src/Animations/DestinationA.js
+++ src/Animations/DestinationA.js
@@ -1,7 +1,7 @@
 export const planetAnimationSettings = {
-  initial: { opacity: 0, y: 70 },
-  animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: -70 },
+  initial: { opacity: 0, scale: 0.9 },
+  animate: { opacity: 1, scale: 1 },
+  exit: { opacity: 0, scale: 0.9 },
   transition: { duration: 0.7 },
 };
 
